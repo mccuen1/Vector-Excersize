e6441a98ef015a957852b8d22e699798607a39cd
mccuen1 <mccuen.brodie@gmail.com>
2021-12-02 14:08:40 -0600

    new code

---
+++ vectors/src/main/java/com/vector/CatsCradle.java
@@ -0,0 +1,34 @@
+package com.vector;
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+import java.awt.Container;
+
+public class CatsCradle extends JFrame{
+
+    private static final int FRAME_WIDTH = 600;
+    private static final int  FRAME_HEIGHT = 600;
+    private static final String FRAME_TITLE = "Cat's Cradle";
+    private static final int NUMBER_OF_SIDES = 12;
+
+    public CatsCradle() {
+        this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.setTitle(FRAME_TITLE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container pane = this.getContentPane();
+        CatsCradlePanel panel = new CatsCradlePanel(NUMBER_OF_SIDES);
+        pane.add(panel);
+
+        Timer timer = new Timer(20, panel);
+        timer.start();
+
+        this.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        CatsCradle catsCradle = new CatsCradle();
+    }
+    
+}
--- vectors/src/main/java/com/vector/Vector2D.java
+++ vectors/src/main/java/com/vector/Vector2D.java
@@ -1,18 +1,16 @@
 package com.vector;
 
-
-
 public final class Vector2D {
 
     // instance variables
     private double x;
     private double y;
-    private double h;
+    //private double h;
 
     public Vector2D(double x, double y) {
         this.x = x;
         this.y = y;
-        this.h = 1.0;
+        //this.h = 1.0;
     } // end constructor
 
     public double getX() {
@@ -21,27 +19,59 @@ public final class Vector2D {
     public double getY() {
         return this.y;
     }
-    public double getH() {
-        return this.h;
-    }
+    // public double getH() {
+    //     return this.h;
+    // }
     public void setX(double x) {
         this.x = x;
     }
     public void setY(double y) {
         this.y = y;
     }
-    public void setH(double h) {
-        this.h = h;
-    }
+    // public void setH(double h) {
+    //     this.h = h;
+    // } 
     
-    public double dot(Vector2D other) {
-        return this.x * other.x + this.y * other.y;
+    public double dot(Vector2D v) {
+        return this.x * v.x + this.y * v.y;
     } // end dot(Vector2D)
 
     public double magnitude() {
         // pythagorean theorem
         return Math.sqrt(this.dot(this));
     } // end magnitude
+
+    public Vector2D add(Vector2D v) {
+        return new Vector2D(this.x + v.x, this.y + v.x);
+    } // add (Vector2D )
+
+    public Vector2D scale(double xFactor, double yFactor) {
+        return new Vector2D(xFactor * this.x, yFactor * this.y);
+    } // scale( double )
+
+    public Vector2D scale(double factor) {
+        return new Vector2D(factor * this.x, factor * this.y);
+    } // scale( double )
+
+    public Vector2D rotate(double angle) {
+        double sine = Math.sin(angle);
+        double cosine = Math.cos(angle);
+        
+        double xCoord = cosine * this.getX() - sine * this.getY();
+        double yCoord = sine * this.getX() + cosine * this.getY();
+        
+        return new Vector2D( xCoord, yCoord );
+    } // rotate( double )
+
+    // rotates, scales, and translates a vector
+    public Vector2D rotateScaleTranslate(double angle, double scaleX,
+            double scaleY, double deltaX, double deltaY) {
+
+        Vector2D u = this.rotate(angle);
+        Vector2D v = u.scale(scaleX, scaleY);
+
+        return new Vector2D(v.x + deltaX, v.y + deltaY);
+    } // rotateScaleTranslate( double, double, double, double, double )
 
     @Override
     public String toString() {
@@ -54,7 +84,7 @@ public final class Vector2D {
         System.out.println("u = " + u);
         System.out.println("x component of u = " + u.getX());
         System.out.println("y component of u = " + u.getY());
-        System.out.println("h component of u = " + u.getH());
+        //System.out.println("h component of u = " + u.getH());
 
         Vector2D v = new Vector2D(5, 12);
         System.out.println("dot product = " + u.dot(v));
